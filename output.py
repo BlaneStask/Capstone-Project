a9510dbae52608c8ebdaa38d482ce1ed294e567e
Blane Staskiewicz <44125947+BlaneStask@users.noreply.github.com>
2019-07-03 15:04:26 -0400

    Update output.py

--- output.py
+++ output.py
@@ -1,14 +1,173 @@
-""""""""""
-The average tweet length is:  
+"""""""""""
+SAMPLE DATA:
 
+The average tweet length is:  117.23809523809524
+The longest word in a single tweet is:  https://t.co/oe4w76lFNT
+The average number of followers are:  113.33333333333333
 
+"""""""""""
+"""""""""""
+TWITTER DATA:
 
-
-
-
-
-
-
-
-
+The average tweet length by word is:  16.6
+"""""""""""
+"""""""""""
+The longest word in the tweets is: 
+https://t.co/PKpojoqMkd
+"""""""""""
+"""""""""""
+The average number of followers are:  726.2
+The average tweet length by char is:  103.6
+The percent of tweets with an # are:  50.0
+The percent of tweets with a @ are:  85.0
+The percent of tweets with punctuation are:  55.0
+"""""""""""
+"""""""""""
+The shortest words in the tweets are: 
+I
+f...
+3
+i
+a
+I
+we
+a
+I
+we
+3D
+#...
+In
+#...
+RT
+AI
+3D
+RT
+of
+#...
+in
+I'm
+I
+the
+or
+-
+on
+"""""""""""
+"""""""""""
+The 100 most common words are: 
+RT :  15
+Python :  14
+#python :  8
+and :  6
+for :  5
+#javascript :  5
+#reactjs :  5
+#programming :  5
+in :  4
+python :  4
+is :  4
+to :  4
+from :  4
+the :  4
+of :  4
+@ProgrammerBooks :  4
+[PDF] :  4
+==&gt; :  4
+#angular :  4
+#vuejs :  4
+I :  3
+this :  3
+my :  3
+The :  3
+you :  3
+learning :  3
+back :  3
+a :  3
+#DataScience :  3
+data :  3
+Testing :  3
+Cookbook :  3
+2nd :  3
+Edition :  3
+https//tco/Tu4ASM6hEl :  3
+#... :  3
+UHHHH :  3
+think :  3
+I'm :  3
+gonna :  3
+need :  3
+more :  3
+ammo :  3
+#Gamedev :  3
+#indiedev :  3
+#IndieGame :  3
+#IndieGameDev :  3
+at :  2
+-a :  2
+can :  2
+learn :  2
+your :  2
+code :  2
+Hunting :  2
+on :  2
+@Stata :  2
+Run :  2
+within :  2
+Stata :  2
+Pass :  2
+information :  2
+forth :  2
+Call :  2
+packages :  2
+machine :  2
+3D :  2
+graphics :  2
+scra... :  2
+Data :  2
+@LateTyping :  2
+#programmer :  2
+#programmers... :  2
+@gp_pulipaka :  2
+AI :  2
+#BigData :  2
+#Analytics :  2
+with :  2
+&amp; :  2
+Things :  1
+want :  1
+rly :  1
+badly :  1
+point :  1
+life :  1
+"""""""""""
+"""""""""""
+The 100 most common symbols are: 
+  :  391
+# :  78
+/ :  45
+. :  44
+: :  35
+$ :  33
+... :  20
+@ :  19
+, :  11
+- :  8
+= :  8
+' :  6
+[ :  6
+] :  6
+& :  6
+; :  6
+" :  2
+_ :  2
+` :  1
+? :  1
+"""""""""""
+"""""""""""
+The user with the most amount of tweets and retweets is: 
+MattBachmann  with  7626
+The average number of tweets and retweets is: 
+3884.0
+"""""""""""
+"""""""""""
+The hour with the greatest number of tweets is:  15
 """""""""""
